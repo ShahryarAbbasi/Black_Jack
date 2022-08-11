5e199896ee94c7e8fa8096898e624b9c22205d7f
ShahryarAbbasi <shizzleabbasi@gmail.com>
2022-08-11 12:04:06 -0400

    add feature that hits dealer with another card if < 17

--- main.js
+++ main.js
@@ -48,22 +48,17 @@ hitButton.addEventListener('click', hitCard)
 stayButton.addEventListener('click', stay)
 
 function start() {
-    flippedCard = deck.pop();
-    // dealerSum += valueOfCard(flippedCard);
-    
     
    for(i = 0; i < 2; i++) {
         let currentCard  = deck.pop();
         dealerSum += valueOfCard(currentCard);
         document.getElementById('dealer').append(currentCard); 
     } 
-    document.getElementById('dealersum').append(dealerSum);
     for (i = 0; i < 2; i++) {
        let currentCard = deck.pop();
        userSum += valueOfCard(currentCard);
         document.getElementById('user').append(currentCard);
     }
-    document.getElementById('usersum').append(userSum)
     
 
 }
@@ -72,7 +67,7 @@ function hitCard() {
     if (hitAllowed === true) {
         let newCard = deck.pop();
         userSum += valueOfCard(newCard);
-    document.getElementById('user').append(newCard);
+        document.getElementById('user').append(newCard);
     }
     
     if (userSum > 21) {
@@ -82,22 +77,58 @@ function hitCard() {
 
 function stay() {
 
+    if (dealerSum < 17) {
+        let currentCard  = deck.pop();
+        dealerSum += valueOfCard(currentCard);
+        document.getElementById('dealer').append(currentCard)
+    } 
+    
+    hitAllowed = false;
+    
+    let message = "";
+    if (userSum > 21) {
+        message = "You Lose!";
+    }
+    else if (dealerSum > 21) {
+        message = "You win!";
+    }
+    else if (userSum == dealerSum) {
+        message = "Tie!";
+    }
+    else if (userSum > dealerSum) {
+        message = "You Win!";
+    }
+    else if (userSum < dealerSum) {
+        message = "You Lose!";
+    }
+    document.getElementById('message').innerText = message;
+    document.getElementById('usersum').innerText = "Sum: " + userSum;
+    document.getElementById('dealersum').innerText = "Sum: " + dealerSum;
 }
 
-function ace() {
-
-}
+// function ace() {
+// if(dealerSum > 21){
+//     valueOfCard()
+// }
+// }
 
 function valueOfCard(evt) {
     let indexOfCard = evt.split(" of ");
     let value = indexOfCard[0];
     if (value == "Ace") {
-        return 11;
+        if (dealerSum < 21) 
+        {return 11;} else {
+            return 1;
+        }
+        if (userSum < 21) {
+            return 11;
+        } else {
+            return 1;
+        }
     } else if (value == "King" || value == "Jack" || value == "Queen") {
         return 10;
     }
     
     return parseInt(value);
 }
-
 
